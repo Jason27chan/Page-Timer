caa7cb092ed5e2940d7f5ff28be8d6c54d123f3d
Jason Chan <jason27chan@gmail.com>
2019-10-21 13:15:15 -0400

    add a graph

--- frontend/src/results.js
+++ frontend/src/results.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Bar} from 'react-chartjs-2';
 
 class Results extends React.Component {
     constructor(props) {
@@ -8,7 +9,31 @@ class Results extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+                <Bar
+                    data={
+                        {
+                            labels: ['a' ,'b', 'c', 'd', 'e'],
+                            datasets: [{
+                                label: "Time sent per page",
+                                fill: false,
+                                backgroundColor: 'rgba(75,192,192,0.4)',
+                                borderColor: 'rgba(75,192,192,1)',
+                                data: [1, 5, 7, 3, 2]
+                            }]
+                        }} 
+                    height={500}
+                  className = "barGraph"
+                  options={{
+  	                maintainAspectRatio: false,
+                        scales: {
+                            yAxes:[{
+                                ticks: {
+                                    suggestedMin:0
+                                }
+                            }]
+                        }
+                     }}
+                />
             </div>
         )
     };
